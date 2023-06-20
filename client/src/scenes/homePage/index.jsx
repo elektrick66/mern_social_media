1a06363a255c4c27036702dc001e47954ddfb4f0
elektrick66 <louaymoussa2001@gmail.com>
2023-06-20 03:42:46 +0100

    FriendList Widget + integration

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,15 +1,16 @@
-import { Box, useMediaQuery } from '@mui/material';
-import { useSelector } from 'react-redux';
-import Navbar from 'scenes/navbar';
-import UserWidget from 'scenes/widgets/UserWidget';
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
+import Navbar from "scenes/navbar";
+import UserWidget from "scenes/widgets/UserWidget";
 import MyPostWidget from "scenes/widgets/MyPostWidget";
-import PostsWidget from 'scenes/widgets/PostsWidget';
-import AdvertWidget from 'scenes/widgets/AdvertWidget';
+import PostsWidget from "scenes/widgets/PostsWidget";
+import AdvertWidget from "scenes/widgets/AdvertWidget";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
 
 const HomePage = () => {
-  const isNonMobileScreens = useMediaQuery("(min-width: 1000px");
-  const { _id, picturePath } = useSelector((state)=> state.user);
- 
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
+
   return (
     <Box>
       <Navbar />
@@ -34,11 +35,12 @@ const HomePage = () => {
           <Box flexBasis="26%">
             <AdvertWidget />
             <Box m="2rem 0" />
+            <FriendListWidget userId={_id} />
           </Box>
         )}
       </Box>
     </Box>
-  )
-}
+  );
+};
 
 export default HomePage;
